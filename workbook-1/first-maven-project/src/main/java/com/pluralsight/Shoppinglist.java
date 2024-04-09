0cf389b7b7d7f4887a9e9de4cc12593f6a9d044a
Hbashir05 <hbashir020505@gmail.com>
2024-04-09 11:45:52 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/Shoppinglist.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Shoppinglist {
+
+    public static void main(String[]args){
+        System.out.println("Item 1");
+        System.out.println("Item 2");
+        System.out.println("Item 3");
+        System.out.println("Item 4");
+        System.out.println("Item 5");
+        System.out.println("Item 6");
+        System.out.println("Item 7");
+        System.out.println("Item 8");
+        System.out.println("Item 9");
+        System.out.println("Item 10");
+    }
+
+}
