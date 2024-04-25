d73447f2d633769f8256a4ac5a93a24e9faa7ade
Hbashir05 <hbashir020505@gmail.com>
2024-04-25 18:29:24 -0400

    Week 3 FamousQuote BedtimeStories PayrollCalculator SearchInventory

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # java-development
+
+# Hadi Bashir
+
+Contact details:
+
+hbashir@appdev.yearup.org
+(704)-607-5970
+
+# Skill List
+
+- Java
+- SQL
+- Excel
+- Business Communication
+- Mircrosoft 365
+- Zoom
+- C++
---
+++ workbook-3/math.java
@@ -0,0 +1,11 @@
+public class math {
+    public static void main(String[] args) {
+        int x = 9;
+        if (x++ < 10) {
+            System.out.println("low");
+        }
+        else {
+            System.out.println("high");
+        }
+    }
+}
